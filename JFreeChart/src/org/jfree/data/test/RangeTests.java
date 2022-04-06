3615ab15a10e9098ad9b51f8c36ea9b337ea943f
Rowan Adair <radair10@qub.ac.uk>
2022-04-06 21:04:05 +0100

    Added Epsilon Values

--- JFreeChart/src/org/jfree/data/test/RangeTests.java
+++ JFreeChart/src/org/jfree/data/test/RangeTests.java
@@ -1,11 +1,12 @@
 package org.jfree.data.test;
 
-
 import junit.framework.TestCase;
 import org.jfree.data.*;
 import org.junit.Test;
 
 public class RangeTests extends TestCase {
+
+    public static final double  EPSILON = 0.0000001d;
 
     @Test
     public void testCreateRange() {
@@ -40,62 +41,75 @@ public class RangeTests extends TestCase {
     @Test
     public void testGetLowerBound() {
         Range r = new Range(1.0, 2.0);
-        assertEquals(1.0, r.getLowerBound(), 0.0);
+        assertEquals(1.0, r.getLowerBound(), EPSILON);
     }
 
     // TC 2: Test getLowerBound() with negative lower bound
     @Test
     public void testGetNegativeLowerBound() {
         Range r = new Range(-1.0, 2.0);
-        assertEquals(-1.0, r.getLowerBound(), 0.0);
+        assertEquals(-1.0, r.getLowerBound(), EPSILON);
     }
 
     // TC 3: Test getLowerBound() with negative range
     @Test
     public void testGetLowerBoundNegativeRange() {
         Range r = new Range(-2.0, -1.0);
-        assertEquals(-2.0, r.getLowerBound(), 0.0);
+        assertEquals(-2.0, r.getLowerBound(), EPSILON);
     }
 
     // TC 4: Test getLowerBound() with zero lower margin
     @Test
     public void testGetLowerBoundEqualsZero() {
         Range r = new Range(0.0, 1.0);
-        assertEquals(0.0, r.getLowerBound(), 0.0);
+        assertEquals(0.0, r.getLowerBound(), EPSILON);
     }
 
+    // TC 5 : Test getLowerBound() does not return upper bound
+    @Test
+    public void testGetLowerBoundDoesNotReturnUpperBound() {
+        Range r = new Range(0.0, 1.0);
+        assertFalse(r.getLowerBound() == 1.0);
+    }
+
+    // *********************************************************************************************
+    // Tests for get upper bound
     // TC 1: Test getUpperBound() valid
     @Test
     public void testGetUpperBoundValid() {
         Range r = new Range(1.0, 2.0);
-        assertEquals(2.0, r.getUpperBound(), 0.0);
+        assertEquals(2.0, r.getUpperBound(), EPSILON);
     }
 
+    // TC 2: Test getUpperBound() with negative lower and upper margins
     @Test
     public void testGetNegativeUpperBound() {
         Range r = new Range(-3.0, -1.0);
-        assertEquals(-1.0, r.getUpperBound(), 0.0);
+        assertEquals(-1.0, r.getUpperBound(), EPSILON);
     }
 
-    @Test
-    public void testGetUpperBoundNegativeRange() {
-        Range r = new Range(-2.0, -1.0);
-        assertEquals(-1.0, r.getUpperBound(), 0.0);
-    }
-
+    // TC 3: Test getUpperBound() with negative range
     @Test
     public void testGetUpperBoundEqualsZero() {
         Range r = new Range(-1.0, 0.0);
-        assertEquals(0.0, r.getUpperBound(), 0.0);
+        assertEquals(0.0, r.getUpperBound(), EPSILON);
     }
 
+    // TC 4 : Test getUpperBound() does not return lower bound
+    @Test
+    public void testGetUpperBoundDoesNotReturnLowerBound() {
+        Range r = new Range(0.0, 1.0);
+        assertFalse(r.getUpperBound() == 0.0);
+    }
+
+    //*********************************************************************************************
     // Tests for expand()
     // TC 1: Test expand() with positive lower and upper margins
     @Test
     public void testExpand() {
         Range r = new Range(1.0, 2.0);
         Range.expand(r, 0.0, 3.0);
-        assertEquals(4.0, r.getLength(), 0.0);
+        assertEquals(4.0, r.getLength(), EPSILON);
     }
 
     // TC 2: Test expand() with negative lower and upper margins
@@ -103,7 +117,7 @@ public class RangeTests extends TestCase {
     public void testExpandNegativeLowerMargin() {
         Range r = new Range(1.0, 2.0);
         Range.expand(r, -1.0, 3.0);
-        assertEquals(4.0, r.getLength(), 0.0);
+        assertEquals(4.0, r.getLength(), EPSILON);
     }
 
     // TC 3: Test expand() with zero lower margin
@@ -111,7 +125,7 @@ public class RangeTests extends TestCase {
     public void testExpandZeroLowerMargin() {
         Range r = new Range(1.0, 2.0);
         Range.expand(r, 0.0, 3.0);
-        assertEquals(4.0, r.getLength(), 0.0);
+        assertEquals(4.0, r.getLength(), EPSILON);
     }
 
     // TC 4: Test expand() with zero upper margin
@@ -119,7 +133,7 @@ public class RangeTests extends TestCase {
     public void testExpandZeroUpperMargin(){
         Range r = new Range(1.0, 2.0);
         Range.expand(r, 0.0, -1.0);
-        assertEquals(4.0, r.getLength(), 0.0);
+        assertEquals(4.0, r.getLength(), EPSILON);
     }
 
     // TC 5: Test expand() with zero lower and upper margins
@@ -145,13 +159,14 @@ public class RangeTests extends TestCase {
         }
     }
 
+    //*********************************************************************************************
     // Test for expandToInclude()
     // TC 1: Test expandToInclude() with positive lower and upper margins
     @Test
     public void testExpandToInclude() {
         Range r = new Range(1.0, 2.0);
         Range.expandToInclude(r, 0.0);
-        assertEquals(3.0, r.getLength(), 0.0);
+        assertEquals(3.0, r.getLength(), EPSILON);
     }
 
     // TC 2: Test expandToInclude() with negative lower and upper margins
@@ -159,7 +174,7 @@ public class RangeTests extends TestCase {
     public void testExpandToIncludeNegativeLowerMargin() {
         Range r = new Range(1.0, 2.0);
         Range.expandToInclude(r, -1.0);
-        assertEquals(3.0, r.getLength(), 0.0);
+        assertEquals(3.0, r.getLength(), EPSILON);
     }
 
     // TC 3: Test expandToInclude() with negative upper margin
@@ -167,7 +182,7 @@ public class RangeTests extends TestCase {
     public void testExpandToIncludeNegativeUpperMargin(){
         Range r = new Range(1.0, 2.0);
         Range.expandToInclude(r, 3.0);
-        assertEquals(3.0, r.getLength(), 0.0);
+        assertEquals(3.0, r.getLength(), EPSILON);
     }
 
     // TC 4: Test expandToInclude() with zero lower and upper margins
@@ -175,7 +190,7 @@ public class RangeTests extends TestCase {
     public void testExpandToIncludeNegativeLowerAndUpperMargin(){
         Range r = new Range(1.0, 2.0);
         Range.expandToInclude(r, -1.0);
-        assertEquals(3.0, r.getLength(), 0.0);
+        assertEquals(3.0, r.getLength(), EPSILON);
     }
 
     // TC 5: Test expandToInclude() with zero lower margin
@@ -183,7 +198,7 @@ public class RangeTests extends TestCase {
     public void testExpandToIncludeZeroLowerMargin() {
         Range r = new Range(1.0, 2.0);
         r = Range.expandToInclude(r, 0.0);
-        assertEquals(2.0, r.getLength(), 0.0);
+        assertEquals(2.0, r.getLength(), EPSILON);
     }
 
     // TC 6: Test expandToInclude() with zero upper margin
@@ -191,9 +206,10 @@ public class RangeTests extends TestCase {
     public void testExpandToIncludeZeroUpperMargin(){
         Range r = new Range(1.0, 2.0);
         r = Range.expandToInclude(r, 3.0);
-        assertEquals(2.0, r.getLength(), 0.0);
+        assertEquals(2.0, r.getLength(), EPSILON);
     }
 
+    //*********************************************************************************************
     // Test for contains()
     // TC 1: Test contains() with positive lower and upper margins
     @Test
@@ -244,6 +260,7 @@ public class RangeTests extends TestCase {
         assertTrue(r.contains(2.0));
     }
 
+    //*********************************************************************************************
     // Tests for combine()
 
     // TC 1: Test combine() lower range first higher range second
@@ -252,8 +269,8 @@ public class RangeTests extends TestCase {
         Range r1 = new Range(1.0, 2.0);
         Range r2 = new Range(3.0, 4.0);
         Range r3 = Range.combine(r1, r2);
-        assertEquals(1.0, r3.getLowerBound(), 0.0);
-        assertEquals(4.0, r3.getUpperBound(), 0.0);
+        assertEquals(1.0, r3.getLowerBound(), EPSILON);
+        assertEquals(4.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 2: Test combine() higher range first lower range second
@@ -262,8 +279,8 @@ public class RangeTests extends TestCase {
         Range r1 = new Range(3.0, 4.0);
         Range r2 = new Range(1.0, 2.0);
         Range r3 = Range.combine(r1, r2);
-        assertEquals(1.0, r3.getLowerBound(), 0.0);
-        assertEquals(4.0, r3.getUpperBound(), 0.0);
+        assertEquals(1.0, r3.getLowerBound(), EPSILON);
+        assertEquals(4.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 3: Test combine() negative and positive ranges
@@ -272,8 +289,8 @@ public class RangeTests extends TestCase {
         Range r1 = new Range(-1.0, 2.0);
         Range r2 = new Range(3.0, 4.0);
         Range r3 = Range.combine(r1, r2);
-        assertEquals(-1.0, r3.getLowerBound(), 0.0);
-        assertEquals(4.0, r3.getUpperBound(), 0.0);
+        assertEquals(-1.0, r3.getLowerBound(), EPSILON);
+        assertEquals(4.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 4: Test combine() negative and negative ranges
@@ -282,8 +299,8 @@ public class RangeTests extends TestCase {
         Range r1 = new Range(-2.0, -1.0);
         Range r2 = new Range(-4.0, -3.0);
         Range r3 = Range.combine(r1, r2);
-        assertEquals(-4.0, r3.getLowerBound(), 0.0);
-        assertEquals(-1.0, r3.getUpperBound(), 0.0);
+        assertEquals(-4.0, r3.getLowerBound(), EPSILON);
+        assertEquals(-1.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 5: Test combine() second range null
@@ -291,8 +308,8 @@ public class RangeTests extends TestCase {
     public void testCombineNullRange() {
         Range r1 = new Range(1.0, 2.0);
         Range r3 = Range.combine(r1, null);
-        assertEquals(1.0, r3.getLowerBound(), 0.0);
-        assertEquals(2.0, r3.getUpperBound(), 0.0);
+        assertEquals(1.0, r3.getLowerBound(), EPSILON);
+        assertEquals(2.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 6: Test combine() first range null
@@ -300,8 +317,8 @@ public class RangeTests extends TestCase {
     public void testCombineNullRange2() {
         Range r1 = new Range(1.0, 2.0);
         Range r3 = Range.combine(null, r1);
-        assertEquals(1.0, r3.getLowerBound(), 0.0);
-        assertEquals(2.0, r3.getUpperBound(), 0.0);
+        assertEquals(1.0, r3.getLowerBound(), EPSILON);
+        assertEquals(2.0, r3.getUpperBound(), EPSILON);
     }
 
     // TC 7: Test combine() both ranges null
